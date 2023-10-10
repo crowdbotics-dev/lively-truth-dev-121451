82888859738fb1905801595cbd2059d33eb3e1c4
Crowdbotics <engineering@crowdbotics.com>
2023-10-10 10:32:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-truth-dev-121451",
+  url: "https://lively-truth-dev-121451.botics.co",
+  api: "https://lively-truth-dev-121451.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
